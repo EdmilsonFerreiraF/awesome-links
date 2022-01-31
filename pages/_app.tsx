10745471f52cb9dadc7894d3ac987b69a33ddbc5
EdmilsonFerreiraF <Edmilsonf.o3@gmail.com>
2022-01-31 15:52:39 -0300

    🚀 :rocket: [Add] User auth0 provider
    Give all pages access to user's authentication state

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,21 @@
+// pages/_app.tsx
+import { UserProvider } from '@auth0/nextjs-auth0'
 import { ApolloProvider } from '@apollo/client'
 
-import Layout from '../components/Layout';
+import Layout from '../components/Layout'
 import apolloClient from '../lib/apollo'
-import '../styles/tailwind.css';
+import '../styles/tailwind.css'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ApolloProvider client={apolloClient}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </ApolloProvider>
-  );
+    <UserProvider>
+      <ApolloProvider client={apolloClient}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ApolloProvider>
+    </UserProvider>
+  )
 }
 
-export default MyApp;
+export default MyApp
